5a2c6a5bc93b4748f8ee821984ee8a6412616a5a
Surya <surya@thesilverlabs.com>
2022-10-03 18:36:12 +0530

    Added TextFields

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,22 +1,8 @@
 import type { NextPage } from 'next';
-import Checkbox from '../components/checkbox';
+import TextField from '../components/noTitleField';
 
 const Test: NextPage = () => {
-  return (
-    <Checkbox>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque est quasi
-      magnam, iure deserunt fuga enim pariatur quaerat labore perspiciatis
-      mollitia error eaque quidem a, culpa, minima sequi porro non? Illo
-      veritatis maiores, quo, voluptatem, saepe quasi minima debitis illum
-      voluptas ad amet. Dolorem, dicta debitis. Vero magni expedita minima
-      perferendis sunt molestias ullam deserunt quam veniam sed cumque tenetur
-      quidem aliquid, praesentium sapiente blanditiis animi quae fuga velit
-      perspiciatis optio obcaecati adipisci assumenda excepturi? Provident,
-      maiores sint. Aliquid nam eius eaque quae soluta at, porro molestias
-      numquam impedit, accusamus atque voluptatum pariatur perspiciatis deserunt
-      iste similique aliquam voluptas rerum!
-    </Checkbox>
-  );
+  return <TextField name="Title" placeholder="e.g. Take coffee break" />;
 };
 
 export default Test;
